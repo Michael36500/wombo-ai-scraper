7b008ed176a4c0aec6e84c338c9b3a2bcc278388
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-06-17 20:28:02 +0200

    another working, now fully auto

--- main.py
+++ main.py
@@ -25,9 +25,17 @@ time.sleep(1)
 # for a in inputs:
 lpnb = 0
 while True:
+    minute = 24
+    while True:
+        now = datetime.datetime.now().time()
+        if now.minute == minute:
+            break
+        time.sleep(10)
+        print("waiting, going on {}, now is {}, means in {}".format(minute, now.minute, 60 - (now.minute + 60 - minute)))
+
     files = os.listdir("Download/")
     for file in files:
-        os.remove(file)
+        os.remove("Download/" + file)
     download.downloadImage("Steampunk","{}".format(gn_inp.gn_inp_short()))
     run_num = datetime.datetime.now()
     # x = time.time()
@@ -53,8 +61,13 @@ while True:
     bot.upload(path)
 
     lpnb += 1
+    path = os.listdir("Download/")
+    path = str(path)
+    path = path.replace("[", "")
+    path = path.replace("]", "")
+    path = path.replace("'", "")
+    print(path)
+    os.remove("Download/" + path)
 
-    os.remove(path)
-
-    for a in range(60):
-        wait.wait(a, run_num)
+    # for a in range(60):
+    #     wait.wait(a, run_num)
